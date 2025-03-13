60c348ec1d87f6cb2bf0f19444b27c12af575f79
Juan Carlos <jbarcinilla@outlook.com>
2025-03-13 17:32:12 -0500

    implement OpenAPI definition  v0

--- ms-library/src/main/java/com/holding/library/controlles/BookController.java
+++ ms-library/src/main/java/com/holding/library/controlles/BookController.java
@@ -3,6 +3,7 @@ package com.holding.library.controlles;
 
 import com.holding.library.entity.BookEntity;
 import com.holding.library.services.BookServices;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,33 +11,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/api/holding")
 @CrossOrigin(origins = "http://localhost:4200") // Permitir Angular
+@Tag(name = "Ejemplo API", description = "Operaciones de prueba")
 public class BookController {
 
     @Autowired
     private BookServices bookService;
 
-    /**
-     * Guarda un nuevo libro en la base de datos.
-     *
-     * @param newBook Información del nuevo libro a guardar.
-     * @return ResponseEntity con estado HTTP 201 (CREATED) si el libro se guarda exitosamente.
-     */
     @PostMapping()
+    @Tag(name = "Save a new book", description = "Provide a book to be saved")
     public ResponseEntity<Void> saveBook(@RequestBody BookEntity newBook) {
         bookService.saveBook(newBook);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    /**
-     * Actualiza la información de un libro existente.
-     *
-     * @param book Información del libro a actualizar.
-     * @return ResponseEntity con estado HTTP 200 (OK) si el libro se actualiza exitosamente, o 204 (NO CONTENT) si no se encuentra el libro.
-     */
     @PutMapping("/updateBook")
+    @Tag(name = "Update an existing book", description = "Provide a book to be updated")
     public ResponseEntity<Void> updateBook(@RequestBody BookEntity book) {
         BookEntity update = bookService.saveBook(book);
         if (update != null) {
@@ -46,25 +39,15 @@ public class BookController {
         }
     }
 
-    /**
-     * Elimina un libro por su ID.
-     *
-     * @param id ID del libro a eliminar.
-     * @return ResponseEntity con estado HTTP 200 (OK) si el libro se elimina exitosamente.
-     */
     @DeleteMapping("/{id}")
+    @Tag(name = "Delete a book by ID", description = "Provide an ID to delete a specific book")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    /**
-     * Obtiene la información de un libro por su ID.
-     *
-     * @param id ID del libro a obtener.
-     * @return ResponseEntity con estado HTTP 200 (OK) si se encuentra el libro, o 204 (NO CONTENT) si no se encuentra el libro.
-     */
     @GetMapping("/{id}")
+    @Tag(name = "Get a book by ID", description = "Provide an ID to retrieve a specific book")
     public ResponseEntity<Void> getBookId(@PathVariable Long id) {
         Optional<BookEntity> op = bookService.getBookById(id);
         if (op.isPresent()) {
@@ -73,16 +56,9 @@ public class BookController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    /**
-     * Obtiene una lista de todos los libros en la base de datos.
-     *
-     * @return Lista de todos los libros.
-     */
     @GetMapping("/list")
+    @Tag(name = "Get a list of all books")
     public List<BookEntity> list() {
         return bookService.getAllBook();
     }
-
-
-
 }
